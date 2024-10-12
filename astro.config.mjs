627a08dcde72325506bf04082ee82446ae154e85
Andrew Daniel <andydotdaniel@gmail.com>
2024-10-12 16:40:26 +0700

    Add server config for dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,10 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jajanku.app',
+
+  // To enable serving the site to other devices on network when developing
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  }
 });
