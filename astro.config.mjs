00764f02660cc99c1e9a94f9799f718847a1f104
Andrew Daniel <andydotdaniel@gmail.com>
2024-10-30 18:33:03 +0700

    Add astro node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jajanku.app',
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
 
   // To enable serving the site to other devices on network when developing
   server: {
