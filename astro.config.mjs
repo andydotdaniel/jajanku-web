843577462bf923847fa56696e1c888a37e169b2a
Andrew Daniel <andydotdaniel@gmail.com>
2024-10-30 21:47:00 +0700

    Use static pages for multilang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jajanku.app',
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "id"]
+  },
 
   // To enable serving the site to other devices on network when developing
   server: {
